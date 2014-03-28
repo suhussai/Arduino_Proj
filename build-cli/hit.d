0cf472aca97ad2d3f921b38b5f7cbf23dd3268d5
Umair <suhussai@ualberta.ca>
2014-03-27 18:31:53 -0600

    final bit

---
+++ build-cli/hit.d
@@ -0,0 +1,16 @@
+build-cli/hit.o: hit.cpp definitions.h \
+  /home/student/arduino-ua/arduino-1.0.1-64/hardware/arduino/cores/arduino/Arduino.h \
+  /home/student/arduino-ua/arduino-1.0.1-64/hardware/arduino/cores/arduino/binary.h \
+  /home/student/arduino-ua/arduino-1.0.1-64/hardware/arduino/cores/arduino/WCharacter.h \
+  /home/student/arduino-ua/arduino-1.0.1-64/hardware/arduino/cores/arduino/WString.h \
+  /home/student/arduino-ua/arduino-1.0.1-64/hardware/arduino/cores/arduino/HardwareSerial.h \
+  /home/student/arduino-ua/arduino-1.0.1-64/hardware/arduino/cores/arduino/Stream.h \
+  /home/student/arduino-ua/arduino-1.0.1-64/hardware/arduino/cores/arduino/Print.h \
+  /home/student/arduino-ua/arduino-1.0.1-64/hardware/arduino/cores/arduino/Printable.h \
+  /home/student/arduino-ua/arduino-1.0.1-64/hardware/arduino/cores/arduino/new.h \
+  /home/student/arduino-ua/arduino-1.0.1-64/hardware/arduino/variants/mega/pins_arduino.h \
+  /home/student/arduino-ua/arduino-1.0.1-64/libraries/Adafruit_GFX/Adafruit_GFX.h \
+  /home/student/arduino-ua/arduino-1.0.1-64/hardware/arduino/cores/arduino/Print.h \
+  /home/student/arduino-ua/arduino-1.0.1-64/libraries/Adafruit_ST7735/Adafruit_ST7735.h \
+  /home/student/arduino-ua/arduino-1.0.1-64/libraries/SPI/SPI.h \
+  functions.h
